acb8c614bd0d6d1c5bb2ad72a0d856be07d31c50
Jim O'Halloran <jim@aligent.com.au>
2016-09-02 11:33:46 +0930

    Prevent _getPageItems from failing if JR_CleverCms isn't installed

--- app/code/local/VF/CustomMenu/Block/Navigation.php
+++ app/code/local/VF/CustomMenu/Block/Navigation.php
@@ -245,9 +245,18 @@ class VF_CustomMenu_Block_Navigation extends Mage_Core_Block_Template
         /** @var Mage_Cms_Model_Page $oParentPage */
         $oParentPage = Mage::getModel('cms/page')->load($item->getCmsPageId());
         if($oParentPage->getId() && $oParentPage->getIsActive()){
-            $cChildPages = $oParentPage->getChildren()
-                ->addFieldToFilter('is_active', array('eq' => 1))
-                ->addFieldToFilter('include_in_menu', array('eq' => 1));
+            $cChildPages = $oParentPage->getChildren();
+
+            // Prevent this function from fataling if JR_CleverCms isn't installed.
+            // ->getChildren() returns null without CleverCms (Varien_Object magic
+            // method) so calling ->addFieldToFilter fails.
+            if ($cChildPages != null) {
+                $cChildPages->addFieldToFilter('is_active', array('eq' => 1))
+                    ->addFieldToFilter('include_in_menu', array('eq' => 1));
+            }  else {
+                $cChildPages = array();
+            }
+
             if(count($cChildPages)){
                 $vCurrentUrl = Mage::helper('core/url')->getCurrentUrl();
                 foreach($cChildPages as $oChildPage){
